31935f2f03243056566dadbb6c7f2dd86d3aaf33
Aditya Dubey <sinister.dubey@gmail.com>
2019-04-06 23:40:59 +0530

    updated the path from dist to build

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 var app = express();
-app.use(serveStatic(path.join(__dirname, "/dist")));
+app.use(serveStatic(path.join(__dirname, "/build")));
 var port = process.env.PORT || 5000;
 app.listen(port, (err) => { console.log("Listening at", port) });
